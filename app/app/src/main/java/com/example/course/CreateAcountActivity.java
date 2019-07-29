23cea213cd5057d53aab648168fdd17dd8b8cb20
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-29 17:21:20 +0530

    Create Signup Activity and fix toolbar issue

---
+++ app/app/src/main/java/com/example/course/CreateAcountActivity.java
@@ -0,0 +1,13 @@
+package com.example.course;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAcountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_acount);
+    }
+}
--- app/app/src/main/java/com/example/course/MainActivity.java
+++ app/app/src/main/java/com/example/course/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.course;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView Signup_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +20,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Signup_text = findViewById(R.id.create_account_text);
+
+        Signup_text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, CreateAcountActivity.class));
             }
         });
     }

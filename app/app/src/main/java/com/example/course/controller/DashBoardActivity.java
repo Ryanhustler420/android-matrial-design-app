fb744d53b9fe4fa7f440c1db55882cd74301183e
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-02 13:15:58 +0530

    Grid And Listview Option Toggle Functionality
    Download Icon From Matrial.io/icons from google
    unzip and past into project

--- app/app/src/main/java/com/example/course/controller/DashBoardActivity.java
+++ app/app/src/main/java/com/example/course/controller/DashBoardActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,6 +17,8 @@ public class DashBoardActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private StaggeredGridLayoutManager staggeredGridLayoutManager;
     private CourseListAdaptor adaptor;
+    private Menu menu;
+    private boolean isListView = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,7 @@ public class DashBoardActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.courseRecyclerView);
         adaptor = new CourseListAdaptor();
 
-        staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        staggeredGridLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(staggeredGridLayoutManager);
 
         recyclerView.setAdapter(adaptor);
@@ -35,5 +39,54 @@ public class DashBoardActivity extends AppCompatActivity {
                 Toast.makeText(DashBoardActivity.this, "Clicked: " + position, Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        this.menu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_toggle) {
+            toggle();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toggle() {
+        if(isListView) {
+            showGridView();
+        }else {
+            showListView();
+        }
+    }
+
+    private void showListView() {
+        staggeredGridLayoutManager.setSpanCount(1);
+        MenuItem item = menu.findItem(R.id.action_toggle);
+        item.setIcon(R.drawable.grid);
+        item.setTitle(getString(R.string.action_show_as_grid));
+        this.isListView = !this.isListView;
+    }
+
+    private void showGridView() {
+        staggeredGridLayoutManager.setSpanCount(2);
+        MenuItem item = menu.findItem(R.id.action_toggle);
+        item.setIcon(R.drawable.bars);
+        item.setTitle(getString(R.string.action_show_as_list));
+        this.isListView = !this.isListView;
     }
 }

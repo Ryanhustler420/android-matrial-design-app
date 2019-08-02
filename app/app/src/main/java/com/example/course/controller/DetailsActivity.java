43afa879b6001ca6701b8974c2f6abaf6d339901
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-02 19:02:46 +0530

    Show Class Image and Information On Details Activity

--- app/app/src/main/java/com/example/course/controller/DetailsActivity.java
+++ app/app/src/main/java/com/example/course/controller/DetailsActivity.java
@@ -1,15 +1,23 @@
 package com.example.course.controller;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.course.R;
+import com.example.course.data.CourseData;
+import com.example.course.model.Course;
 
 public class DetailsActivity extends AppCompatActivity {
 
-    private int courseId;
+    private Course course;
+    private ImageView courseImageView;
+    private TextView courseTitle;
+    private InputMethodManager inputManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +26,20 @@ public class DetailsActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Bundle bundle = getIntent().getExtras();
-        courseId = bundle.getInt("course_id");
-        Toast.makeText(this, "course id: " + courseId, Toast.LENGTH_LONG).show();
+        setupUI();
+        loadCourse();
+    }
 
+    private void loadCourse() {
+        course = new CourseData().courseList().get(getIntent().getExtras().getInt("course_id"));
+        courseImageView.setImageResource(course.getImageRecourseId(this));
+        courseTitle.setText(course.getCourseName());
+    }
+
+    private void setupUI() {
+        inputManager =  (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        courseImageView = findViewById(R.id.detailsCourseImage);
+        courseTitle = findViewById(R.id.detailsCourseTitle);
     }
 
 }
